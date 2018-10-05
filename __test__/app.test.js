bd1101d7607185a679654ee031affc806328b84b
vmontanomena <vicente.montanomena@telefonica.com>
2018-10-05 09:55:52 +0200

    Adding unit test for request

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -8,4 +8,22 @@ describe('Probando el servidor. Response codes 200',() => {
       done();
     })
   })
+
+  test('Se espera un 200 y status true', done => {
+    request(app).get('/').then((response) => {
+      //console.log(response)
+      expect(response.statusCode).toBe(200);
+      //expect(response.body.status).toBe(false); //  te da igual lo que venga solon espera que sea TRUE
+      expect(response.body.message).toBeTruthy(); //  te da igual lo que venga solon espera que sea TRUE
+      done();
+    })
+  })
+
+  test('Se espera un 404 y status true', done => {
+    request(app).get('/error').then((response) => {
+      //console.log(response)
+      expect(response.statusCode).toBe(404);
+      done();
+    })
+  })
 })
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hola Mundo Cruel!')
+    res.status(200).send({message: 'Hola Mundo Cruel!', status: true})
 })
 
 module.exports = app;
